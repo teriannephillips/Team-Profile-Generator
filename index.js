ca2636649bff2660cbe52ad2d1d9a4b37883c62a
Teri Phillips <phillips.terianne@gmail.com>
2023-02-07 09:19:27 -0500

    added classes

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
+const Employee = require('./lib/employee');
 //required packages
 const inquirer = require("inquirer")
 const fs = require('fs');
+const { default: Choices } = require("inquirer/lib/objects/choices");
+
+function employeeQuestions(position){
 inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'Please enter the employees name'
+      message: 'Please enter the ' +  position + 's name'
     },
     {
       type: 'number',
       name: 'id',
-      message: 'Please enter the employees id number'
+      message: 'Please enter the ' + position  + 's id number'
     },
     { type: 'input',
     name: 'email',
-    message: 'Please enter the employees e-mail address'
+    message: 'Please enter the ' + position  + 's e-mail address'
     },
     { type: 'input',
     name: 'number',
-    message: 'Please enter the employees office number'
+    message: 'Please enter the ' + position  + 's office number'
     },
   ]).then(answers => {
     //call the function to write this employee to the readme file
     writeToFile('index.html', answers);
-
+    positionOption();
   });
-  //function to write the README file
+}
+
+function positionOption(){
+  inquirer.prompt([
+      {
+        type: 'list',
+        name: 'position',
+        message: 'Select an option to add another employee',
+        choices: ['done', 'engineer', 'intern']
+      },
+    ]).then(answers => {
+      //call the function to ask the employee questions again
+      if (answers.position = "done") {
+        return;
+      }
+      else {
+      employeeQuestions(answers.position);
+      }
+  
+    });
+  }
+  //function to write the HTML file
 function writeToFile(fileName, answers) {
     console.log(answers);
 }
+let position = "Manager";
+employeeQuestions(position);
