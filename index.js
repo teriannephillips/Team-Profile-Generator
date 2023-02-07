9682625a00be4640ff30d81c5cd2e07e41f48146
Teri Phillips <phillips.terianne@gmail.com>
2023-02-07 09:51:46 -0500

    added questions for engineer and intern

--- index.js
+++ index.js
@@ -4,25 +4,27 @@ const inquirer = require("inquirer")
 const fs = require('fs');
 const { default: Choices } = require("inquirer/lib/objects/choices");
 
-function employeeQuestions(position){
-inquirer.prompt([
+function managerQuestions() {
+  inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'Please enter the ' +  position + 's name'
+      message: 'Please enter the managers name'
     },
     {
       type: 'number',
       name: 'id',
-      message: 'Please enter the ' + position  + 's id number'
+      message: 'Please enter the managers id number'
     },
-    { type: 'input',
-    name: 'email',
-    message: 'Please enter the ' + position  + 's e-mail address'
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter the managers e-mail address'
     },
-    { type: 'input',
-    name: 'number',
-    message: 'Please enter the ' + position  + 's office number'
+    {
+      type: 'input',
+      name: 'number',
+      message: 'Please enter the managers office number'
     },
   ]).then(answers => {
     //call the function to write this employee to the readme file
@@ -30,29 +32,87 @@ inquirer.prompt([
     positionOption();
   });
 }
-
-function positionOption(){
+function engineerQuestions() {
   inquirer.prompt([
-      {
-        type: 'list',
-        name: 'position',
-        message: 'Select an option to add another employee',
-        choices: ['done', 'engineer', 'intern']
-      },
-    ]).then(answers => {
-      //call the function to ask the employee questions again
-      if (answers.position = "done") {
-        return;
-      }
-      else {
-      employeeQuestions(answers.position);
-      }
-  
-    });
-  }
-  //function to write the HTML file
-function writeToFile(fileName, answers) {
-    console.log(answers);
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Please enter the engineers name'
+    },
+    {
+      type: 'number',
+      name: 'id',
+      message: 'Please enter the engineers id number'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter the engineers e-mail address'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Please enter the engineers github username'
+    },
+  ]).then(answers => {
+    //call the function to write this employee to the readme file
+    writeToFile('index.html', answers);
+    positionOption();
+  });
 }
-let position = "Manager";
-employeeQuestions(position);
+function internQuestions() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Please enter the interns name'
+    },
+    {
+      type: 'number',
+      name: 'id',
+      message: 'Please enter the interns id number'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter the interns e-mail address'
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'Please enter the interns school name'
+    },
+  ]).then(answers => {
+    //call the function to write this employee to the readme file
+    writeToFile('index.html', answers);
+    positionOption();
+  });
+}
+function positionOption() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'position',
+      message: 'Select an option to add another employee',
+      choices: ['done', 'engineer', 'intern']
+    },
+  ]).then(answers => {
+    //call the function to ask the employee questions again
+    console.log(answers.position);
+    if (answers.position == "done") {
+      return;
+    }
+    else if (answers.position == "engineer") {
+      engineerQuestions();
+    }
+    else if (answers.position == "intern") {
+      internQuestions();
+    }
+
+  });
+}
+managerQuestions();
+//function to write the HTML file
+function writeToFile(fileName, answers) {
+  console.log(answers);
+}
